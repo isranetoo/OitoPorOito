45a889ac3d72168c91608fc35737adffac063301
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2025-07-26 00:50:21 -0300

    [FEAT] Add PlayerInfo component to display player details; integrate into PlayComputer page for enhanced user experience

--- src/pages/PlayComputer.jsx
+++ src/pages/PlayComputer.jsx
@@ -1,7 +1,9 @@
 
 import React from "react";
+
 import ChessBoard from "../components/PlayComputer/ChessBoard";
 import BotSidebar from "../components/PlayComputer/BotSidebar";
+import PlayerInfo from "../components/PlayComputer/PlayerInfo";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -11,9 +13,30 @@ export default function PlayComputer() {
       <Navbar />
       <main className="flex-1 flex flex-col items-center justify-center w-full py-8">
         <div className="w-full max-w-6xl flex flex-col md:flex-row items-center justify-center gap-0 md:gap-12 px-2 md:px-8">
-          <div className="flex items-center justify-center w-full md:w-auto">
+          {/* Coluna do tabuleiro e infos */}
+          <div className="flex flex-col items-center w-full md:w-auto">
+            {/* Header do bot */}
+            <PlayerInfo
+              avatar="/assets/img/anna-rudolf.jpg"
+              name="Jimmy"
+              rating={600}
+              flagUrl="https://flagcdn.com/w40/us.png"
+              flagAlt="USA"
+              isBot={true}
+            />
+            {/* Tabuleiro */}
             <ChessBoard />
+            {/* Footer do usuário */}
+            <PlayerInfo
+              avatar="/assets/img/puzzle1.png"
+              name="Guest"
+              rating={0}
+              flagUrl={null}
+              flagAlt="Indefinida"
+              isBot={false}
+            />
           </div>
+          {/* Sidebar dos bots */}
           <div className="mt-8 md:mt-0 md:ml-8 flex-shrink-0">
             <BotSidebar />
           </div>
